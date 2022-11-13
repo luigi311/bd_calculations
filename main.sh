824348cbfa651bdd5275f72d51c19544745f7f69
luigi311 <git@luigi311.com>
2022-11-13 03:46:05 -0700

    Reduce parallel encoders. Test decode

--- main.sh
+++ main.sh
@@ -4,10 +4,10 @@ podman image prune -a -f
 podman build -t "bd-compare" .
 
 ENCODERS=("x265" "aomenc")
-VIDEOS=("bbb_short.mkv")
+VIDEOS=("bbb_shorter.mkv")
 
 for ENCODER in "${ENCODERS[@]}"; do
     for VIDEO in "${VIDEOS[@]}"; do
-        podman run --rm -v "${HOME}/Videos:/videos:z" -v "$(pwd):/app:z" bd-compare scripts/run.sh -i "/videos/${VIDEO}" --enc "$ENCODER" --output /videos --bd "steps/quality_${ENCODER}" --preset "steps/preset_${ENCODER}" --resume
+        podman run --rm -v "${HOME}/Videos:/videos:z" -v "$(pwd):/app:z" bd-compare scripts/run.sh -i "/videos/${VIDEO}" --enc "$ENCODER" --output /videos --bd "steps/quality_${ENCODER}" --preset "steps/preset_${ENCODER}" --resume -e 2 --decode
     done
 done
