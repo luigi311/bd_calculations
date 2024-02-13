1384fce389bae519e12995836d897a0d5e1ef69c
Luigi311 <git@luigi311.com>
2024-02-13 16:03:55 -0700

    Add Tears of Steel
    Signed-off-by: Luigi311 <git@luigi311.com>

--- main.sh
+++ main.sh
@@ -97,7 +97,7 @@ done
 update_container_image
 
 ENCODERS=("x265" "aomenc" "rav1e" "svt-av1" "x264")
-VIDEOS=("Big Buck Bunny 720p.mkv" "Big Buck Bunny 1080p.mkv")
+VIDEOS=("Big Buck Bunny 720p.mkv" "Big Buck Bunny 1080p.mkv" "Tears of Steel 720p.mkv" "Tears of Steel 1080p.mkv")
 THREADS=$(nproc --all)
 ENC_WORKERS=1
 
